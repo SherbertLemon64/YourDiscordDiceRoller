90c114a06293707f2fa5195b4bfdfde0efb5e1ce
youngman114 <artur.roos@mail.ru>
2021-02-05 22:14:38 +0300

    :sparkles: Command dispatching

--- __main__.py
+++ __main__.py
@@ -1,13 +1,30 @@
 import discord
 import d20
+from typing import Optional, Callable
+from functools import wraps
 
 from config import token
 
 
 DISCORD_FORMAL_REQUEST_CONTENT_LIMIT = 2000
+PREFIX = '!'
+DISPATCHED = dict()
 
 
 app = discord.Client()
+
+def dispatched(cmd:str, f: Callable) -> Callable:
+    
+    @wraps(f)
+    def wrapper(*args, **kwargs) -> Optional[str]:
+        return f(args, **kwargs)
+
+    DISPATCHED.update({cmd : wrapper})
+    return wrapper
+
+
+# Return Pong if pinged, simple example for dispatching
+dispatched("ping", lambda *_: "Pong!")
 
 
 def dice_roll_string(author, roll) -> str:
@@ -17,6 +34,10 @@ def dice_roll_string(author, roll) -> str:
         return f"{author.mention}: **way result too long.** {roll.total}"
     else:
         return string
+
+
+def dispatch_command(cmd, *args, msg) -> Optional[str]:
+    return DISPATCHED.get(cmd)(*args, msg)
 
 
 @app.event
@@ -30,6 +51,17 @@ async def on_message(msg: discord.message.Message):
     text:str = msg.content.strip()
 
     roll = None
+    command_args = []
+
+    if text.startswith(PREFIX):
+        command_args = text[len(PREFIX):].split(' ')
+        # Slice first, everything else is args, if no args then None
+        cmd, args = command_args[0] if command_args[0:1] else None, command_args[1:] or None
+        response: str = dispatch_command(cmd, args, msg=msg)
+        if response is None:
+            return
+        else:
+            await msg.channel.send(response)
 
     try:
         roll = d20.roll(text)
